fe2c080488db3c9f6c2c19ffc4255d96d20791bc
ncronquist <nick@ncronquist.com>
2015-05-29 23:40:53 -0700

    Rails app created; Gems added

---
+++ Gemfile
@@ -0,0 +1,41 @@
+source 'https://rubygems.org'
+
+
+gem 'rails', '4.2.1'
+
+gem 'rails-api'
+
+gem 'spring', :group => :development
+
+
+gem 'pg'
+
+# oAuth Gems
+gem 'omniauth'
+gem 'omniauth-stackexchange'
+gem 'omniauth-github'
+
+# Makes your rails console WAY prettier
+gem 'pry-rails'
+
+# Paper Trail
+# PaperTrail lets you track changes to your models' data. It's good for auditing
+# or versioning. You can see how a model looked at any stage in its lifecycle,
+# revert it to any version, and even undelete it after it's been destroyed.
+# https://github.com/airblade/paper_trail
+gem 'paper_trail', '~> 4.0.0.rc'
+
+# To use ActiveModel has_secure_password
+# gem 'bcrypt', '~> 3.1.7'
+
+# To use Jbuilder templates for JSON
+# gem 'jbuilder'
+
+# Use unicorn as the app server
+# gem 'unicorn'
+
+# Deploy with Capistrano
+# gem 'capistrano', :group => :development
+
+# To use debugger
+# gem 'ruby-debug19', :require => 'ruby-debug'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+DevBox::Application.config.secret_token = '7f965303beda9ad8e7b19d386232ac076944176e6143c04a831302687128c6c099f387cf73a3f03b953bc0e474e2c7ba043cec323e768bae721e83896f6679fc'
