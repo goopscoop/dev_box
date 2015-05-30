fd726972c754558abc294b3c44c69aeb7d37c226
ncronquist <nick@ncronquist.com>
2015-05-30 00:02:12 -0700

    Create tools model

---
+++ db/migrate/20150530070152_create_tools.rb
@@ -0,0 +1,15 @@
+class CreateTools < ActiveRecord::Migration
+  def change
+    create_table :tools do |t|
+      t.string :title
+      t.text :description
+      t.string :language
+      t.integer :is_free
+      t.string :web_url
+      t.string :repo_url
+      t.integer :avg_rating
+
+      t.timestamps null: false
+    end
+  end
+end
