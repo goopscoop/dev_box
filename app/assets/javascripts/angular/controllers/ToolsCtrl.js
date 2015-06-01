64877ae9f7e3c62c2181d6899b87f0d79f14d543
goopscoop <codybarrus@gmail.com>
2015-06-01 16:52:29 -0700

    can filter by category/search via url

--- app/assets/javascripts/angular/controllers/ToolsCtrl.js
+++ app/assets/javascripts/angular/controllers/ToolsCtrl.js
@@ -1,17 +1,33 @@
-DevBox.controller( 'ToolsCtrl' , [ '$scope' , '$http', '$resource' ,
-  function( $scope , $http , $resource ){
+DevBox.controller( 'ToolsCtrl' , [ '$scope' , '$http', '$resource' , '$location',
+  function( $scope , $http , $resource , $location ){
 
-    // var filterArr = function(array, searchText ){
-    //   return array.filter( function( tool ){
-    //     if ( tool.title.toLowerCase().indexOf(searchText.toLowerCase()) != -1 ) {
-    //       return true;
-    //     } else {
-    //       return false;
-    //     }
-    //   })
-    // }
+    var init = function(){
+      if($location.search()) {
+        get = buildUrl(true, $location.search().q, $location.search().c, $location.search().t)
+          $http.get( get ).success( function( data ){
+          // returns an array of objects with Tools and associated categories and tags
+          $scope.searchTools = data
+        })
+      }
+    }
 
-    $scope.tools = [];
+    // builds url. API is required (true if api call, false if local url)
+    // all other paramaters are optional.
+    var buildUrl = function( api, query, cats, tags ){
+      if ( api ) {
+        var url =  '/api/tools?'
+      } else {
+        url = ''
+      }
+      if ( query ) url += 'q=' + query;
+      if ( (query && cats) || (query && tags) ) url += '&';
+      if ( cats ) url += 'c=' + cats;
+      if ( cats && tags ) url += '&';
+      if ( tags ) url += 't=' + tags;
+      return url
+    }
+
+    $scope.categories = [];
 
     // String the search bar is binding to.
     $scope.toolSearchText;
@@ -28,17 +44,27 @@ DevBox.controller( 'ToolsCtrl' , [ '$scope' , '$http', '$resource' ,
       })
         console.log($scope.searchTools)
         return $scope.searchTools
-
     }
 
     $http.get( '/api/tools' ).success( function( data ){
       // returns an array of objects with Tools and associated categories and tags
-      for( tool in data ){
-        $scope.tools.push(data[tool]);
-      }
+      $scope.categories = data.categories;
+      $scope.tags = data.tags;
       // console.log( $scope.tools )
     })
 
+    $scope.addCat = function(catName){
+
+      get = buildUrl( true, $location.search().q , catName )
+      localUrl = buildUrl( false, $location.search.q, catName)
+      // $http.get( get ).success( function( data ){
+      //   console.log(localUrl)
+      //   // returns an array of objects with Tools and associated categories and tags
+      //   $scope.searchTools = data
+      // })
+        $location.search(localUrl)
+    }
 
 
+    init();
 }]);
--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -12,13 +12,40 @@ class ToolsController < ApplicationController
         return({ id: tool.id, title: tool.title, tags: tags, categories: cats })
   end
 
+
+
   # tools GET
   def index
 
     tool_info = []
     search_term = params[:q]
+    category = params[:c]
+    tag = params[:t]
 
-    if search_term
+    # Search results for a query and category
+    if search_term && category
+      categoryMatch = Category.find_by_category(category)
+      searchMatch = categoryMatch.tools.where('title ilike ?', "%#{search_term}%")
+
+      # Add tools to json + associated tags and categories
+      searchMatch.each do |tool|
+        tool_info.push(add_tool_info tool)
+      end
+
+      render json: tool_info
+    elsif category
+      # Just a category search
+      categoryMatch = Category.find_by_category(category)
+      searchMatch = categoryMatch.tools
+
+      searchMatch.each do |tool|
+        tool_info.push(add_tool_info tool)
+      end
+
+      render json: tool_info
+
+    elsif search_term
+    # Search results for just a query (no categories or tags)
       searchMatch = Tool.where('title ilike ?', "%#{search_term}%")
       tagMatch = Tag.where('tag ilike ?', "%#{search_term}%")
 
@@ -41,28 +68,13 @@ class ToolsController < ApplicationController
         end
 
       end
-        render json: tool_info
+      render json: tool_info
 
     else
 
-      all_tools = Tool.all
-
-      all_tools.each do |tool|
-
-        cats = []
-        tool.categories.each do |cat|
-          cats.push({ id: cat[:id] , category: cat[:category] })
-        end
-
-        tags = []
-        tool.tags.each do |tag|
-          tags.push({ id: tag[:id] , tag: tag[:tag]})
-        end
-
-        tool_info.push({ id: tool.id, title: tool.title, tags: tags, categories: cats })
-      end
-
-      render json: tool_info
+      all_categories = Category.all
+      all_tags = Tag.all
+      render json: { categories: all_categories, tags: all_tags }
 
     end
   end
