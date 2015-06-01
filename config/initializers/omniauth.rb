347a56f65787163f394ccf39cd089c4499eb8edc
ncronquist <nick@ncronquist.com>
2015-06-01 00:32:04 -0700

    Complete oauth configuration

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,11 +6,37 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   }
 
   # StackExchange - StackOverflow
-  # provider :stackexchange, "client_id", "client_secret", public_key: "key", site: 'stackoverflow'
-  provider :stackexchange, ENV['SE_CLIENT_ID'], ENV['SE_CLIENT_SECRET'], public_key: ENV['SE_PUBLIC_KEY'], site: 'stackoverflow'#, {
-  #  redirect_uri: 'http://localhost:3000/auth/github/callback'
-  #}
+  provider :stackexchange, ENV['SE_CLIENT_ID'], ENV['SE_CLIENT_SECRET'], public_key: ENV['SE_PUBLIC_KEY'], site: 'stackoverflow'
 
   # GitLab
+  provider :gitlab, ENV['GITLAB_KEY'], ENV['GITLAB_SECRET']
 
 end
+
+class NonExplodingFailureEndpoint
+  attr_reader :env
+
+  def self.call(env)
+    new(env).call
+  end
+
+  def initialize(env)
+    @env = env
+  end
+
+  def call
+    redirect_to_failure
+  end
+
+  def raise_out!
+    raise env['omniauth.error'] || OmniAuth::Error.new(env['omniauth.error.type'])
+  end
+
+  def redirect_to_failure
+    message_key = env['omniauth.error.type']
+    new_path = "#{env['SCRIPT_NAME']}#{OmniAuth.config.path_prefix}/failure?message=#{message_key}"
+    Rack::Response.new(["302 Moved"], 302, 'Location' => new_path).finish
+  end
+end
+
+OmniAuth.config.on_failure = NonExplodingFailureEndpoint
