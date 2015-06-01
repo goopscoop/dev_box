2bff3cd90840f3d11216f5467d98e7012daa603d
goopscoop <codybarrus@gmail.com>
2015-05-31 21:10:45 -0700

    Tools showing up on tools#index view

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -2,6 +2,24 @@ class ToolsController < ApplicationController
 
   # tools GET
   def index
+    all_tools = Tool.all
+    tool_info = {}
+
+    all_tools.each do |tool|
+
+      cats = []
+      tool.categories.each do |cat|
+        cats.push({ id:cat.id , category:cat.category })
+      end
+
+      tags = []
+      tool.tags.each do |tag|
+        tags.push({ id:tag.id , tag:tag.tag})
+      end
+
+      tool_info[tool.title] = { title: tool.title, tags: tags, categories: cats }
+    end
+    render json: tool_info
   end
 
   # tool GET
