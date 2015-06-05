ccd0bbaef1bac36ffe24056b6166b74366ee3774
goopscoop <codybarrus@gmail.com>
2015-06-04 18:01:42 -0700

    most style

--- app/assets/javascripts/angular/controllers/StaticCtrl.js
+++ app/assets/javascripts/angular/controllers/StaticCtrl.js
@@ -1,3 +1,12 @@
 DevBox.controller('StaticCtrl',['$scope',function($scope){
+  var message = ['Nick has adorable cats','Cody has no cats :(','The deepest whole in the world is in Russia','Deepest hole https://youtu.be/zz6v6OfoQvs']
 
+  // var init = function(){
+  //   var count = 0;
+  //   $interval(function(){
+
+  //     Materialize.toast( message[count], 4000)
+  //     count += 1
+  //   }, 7000);
+  // }
 }]);
