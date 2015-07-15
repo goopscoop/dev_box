7c6e92206d736799b41f71d12db14d6117db591d
goopscoop <codybarrus@gmail.com>
2015-07-15 14:03:57 -0700

    added frontend validation service

--- app/assets/javascripts/angular/controllers/ToolsNewCtrl.js
+++ app/assets/javascripts/angular/controllers/ToolsNewCtrl.js
@@ -1,5 +1,5 @@
-DevBox.controller( 'ToolsNewCtrl', [ '$scope' , '$resource', '$location', '$http',
-  function( $scope, $resource, $location, $http ){
+DevBox.controller( 'ToolsNewCtrl', [ '$scope' , '$resource', '$location', '$http', 'devValidate',
+  function( $scope, $resource, $location, $http, devValidate ){
 
   $scope.showPrinciples = true;
   $scope.readonly = false;
@@ -55,43 +55,39 @@ DevBox.controller( 'ToolsNewCtrl', [ '$scope' , '$resource', '$location', '$http
   }
 
   $scope.saveTool = function() {
-    if( !$scope.title ) {
-      Materialize.toast( 'a title is required', 4000)
-    } else if( !$scope.category01 ){
-      Materialize.toast( 'a category is required', 4000)
-    } else if( !$scope.description ) {
-      Materialize.toast( 'a description is required', 4000)
-    } else if( !$scope.web_url && !$scope.repo_url && !$scope.doc_url ) {
-      Materialize.toast( 'at least one link is required', 4000)
-    } else if( !$scope.tags ) {
-      Materialize.toast( 'at least one tag is required', 4000)
-    } else {
-      var tool = new Tool();
-      tool.title = $scope.title;
-      tool.description = $scope.description;
-      tool.language = $scope.language;
-      tool.is_open = $scope.is_open? true : false;
-      tool.is_free = $scope.is_free;
-      tool.web_url = $scope.web_url;
-      tool.repo_url = $scope.repo_url;
-      tool.doc_url = $scope.doc_url;
-      tool.avg_rating = null;
-      tool.categories = [$scope.category01];
-      tool.tags = $scope.selectedTags;
-      tool.$save(function(data) {
-        console.log("server returns before",data);
-        if(data.result.id === null){
-                 // ... error_message comes from the server
-            Materialize.toast( 'Oops. Something happened', 4000)
-            Materialize.toast( 'Please make sure this tool is a unique entry', 4000)
-             console.log("server returns if",data);
+    if ( devValidate.title($scope.title) &&
+        devValidate.description($scope.description) &&
+        devValidate.category($scope.category01) &&
+        devValidate.tags( $scope.tags ) &&
+        devValidate.urls( $scope.web_url, $scope.repo_url, $scope.doc_url ) ) {
 
-          } else {
+          var tool = new Tool();
+          tool.title = $scope.title;
+          tool.description = $scope.description;
+          tool.language = $scope.language;
+          tool.is_open = $scope.is_open? true : false;
+          tool.is_free = $scope.is_free;
+          tool.web_url = $scope.web_url;
+          tool.repo_url = $scope.repo_url;
+          tool.doc_url = $scope.doc_url;
+          tool.avg_rating = null;
+          tool.categories = [$scope.category01];
+          tool.tags = $scope.selectedTags;
+          tool.$save(function(data) {
+            console.log("server returns before",data);
+            if(data.result.id === null){
+                     // ... error_message comes from the server
+                Materialize.toast( 'Oops. Something happened', 4000)
+                Materialize.toast( 'Please make sure this tool is a unique entry', 4000)
+                 console.log("server returns if",data);
 
-               console.log("server returns else",data);
-            $location.url("/tools/" + data.result.id)
-          }
-      })
+              } else {
+
+                   console.log("server returns else",data);
+                $location.url("/tools/" + data.result.id)
+              }
+          })
+
     }
   }
 
---
+++ app/assets/javascripts/angular/services/devBoxServices.js
@@ -0,0 +1,48 @@
+DevBox.factory('buildUrl', function(){
+     // builds url. API is required (true if api call, false if local url)
+    // all other paramaters are optional.
+  return {
+    build: function( api, query, cats, tags ){
+      if ( api ) {
+        var url =  '/api/tools?'
+      } else {
+        url = ''
+      }
+      if ( query ) url += 'q=' + query;
+      if ( (query && cats) || (query && tags) ) url += '&';
+      if ( cats ) url += 'c=' + cats;
+      if ( cats && tags ) url += '&';
+      if ( tags ) url += 't=' + tags;
+      return url
+    }
+  }
+})
+  .factory('devValidate', function(){
+    var isValidated = function( testEl, errMessage, optionalTestEl, optionalTestEl2 ){
+      if( testEl || optionalTestEl || optionalTestEl2 ) {
+        return true;
+      } else {
+        Materialize.toast( errMessage, 4000)
+        return false;
+      }
+    }
+
+
+    return {
+      title: function( toolTitle ){
+        return isValidated( toolTitle, 'a title is required' );
+      },
+      category: function( toolCategory ){
+        return isValidated( toolCategory, 'a category is required' );
+      },
+      description: function( toolDescription ){
+        return isValidated( toolDescription, 'a description is required' );
+      },
+      urls: function( webUrl, repoUrl, docsUrl ){
+        return isValidated( webUrl, 'at least one link is required', repoUrl, docsUrl );
+      },
+      tags: function( toolTags ){
+        return isValidated(toolTags, 'at least one tag is required')
+      }
+    }
+  })
